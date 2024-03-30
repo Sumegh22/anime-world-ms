5c21e13e59581cb5856a5aba2b314daacfbd12ad
Sumegh from Acer <samytembhurne@gmail.com>
2024-03-31 02:19:32 +0530

    1. externalizing configs using configserver

---
+++ ConfigServer/ConfigServer/src/test/java/com/sgt/config/server/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgt.config.server.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
